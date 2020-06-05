5d95a5c5c41c926fd7c9233c92ce441bb74adc16
Sarah Bates <skbates00@me.com>
2020-06-05 10:04:57 -0500

    everything works, but my darn single delete button...

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,35 +3,47 @@ import './App.css';
 import Todo from './Components/Todo';
 import Tasks from './Components/Tasks';
 
+let keyCount = 0;
+
 function App() {
   const [input, setInput] = useState('');
   const [tasks, setTasks] = useState([]);
+  const [id, setId] = useState('')
   
   const handleInputChange = (event) => {
-    setInput(event.target.value)
+    setInput(event.target.value);
+    setId(event.targetvalue + keyCount);
   }
 
   const handleSubmit = (event) => {
     event.preventDefault();
     if (input !== ''){
-      setTasks(tasks.concat({input}));
+      setTasks(tasks.concat({input, id}));
       setInput('');
+      setId('');
+      keyCount ++;
     }
   }
 
-  const handleDelete = (event) => {
-    event.preventDefault();
-    setTasks(tasks.splice(event.target, 1))
+  const handleDelete = (taskId) => {
+    setTimeout( function() {
+      setTasks (tasks.filter((task) => task.id !== taskId));
+   
+    }, 1000)
   }
 
-  const todoItems = tasks.map((task, index) => <Tasks input = {task.input} key = {task.input + tasks.length} handleDelete = {handleDelete} /> )
+  const handleDeleteAll = () => setTasks([]);
+
+  const todoItems = tasks.map((task, index) => <Tasks input = {task.input} id = {task.id} key = {task.id} handleDelete = {handleDelete} /> )
+  
   return (
     <div className="App">
       <header className="todoHeader">
-        To Do List
+        To Do List:
       </header>
       <Todo handleInputChange = {handleInputChange} handleSubmit = {handleSubmit} input = {input} />
       {todoItems}
+      <button className = "delete-all" onClick={handleDeleteAll}> Delete All Tasks </button>
       
       </div>
   );
--- todo-app/src/Components/Tasks.jsx
+++ todo-app/src/Components/Tasks.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-export default function Tasks({input, handleDelete}){
+export default function Tasks({input, handleDelete, id}){
+
+    const [fadeOut, setFadeOut] = useState(false);
+    const [completed, setCompleted] = useState(false);
+
+    const addFadeOut = () => {
+        setFadeOut(true);
+    }
+
+    const toggleChecked = () => {
+        setCompleted(!completed);
+    }
 
     return(
-        <div className = 'task'>
+        <div className ={fadeOut ? 'task fade-out': 'task'} onClick = {toggleChecked}>
             <p className = 'task-text' >
-                <input type = 'checkbox' className = "task-completed" />
+                <input type = 'checkbox' className = "task-completed" onChange= {toggleChecked} checked = {completed}/>
                 {input}
             </p>
-            <button className = "delete-btn" onClick = {handleDelete} > Delete </button>
+            <button className = "delete-btn" onClick = {() => {handleDelete(id); addFadeOut();}} > Delete </button>
             
         </div>
     )
